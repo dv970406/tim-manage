a735a275a69e22c17f5a9021a25bee2d773e929f
dv970406 <dv970406@gmail.com>
2023-01-05 09:30:31 +0900

    feat: Modify Auth Decorator & Auth Guard

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -9,3 +9,13 @@ export class AuthGuard implements CanActivate {
     return user ? true : false;
   }
 }
+
+@Injectable()
+export class ManagerGuard implements CanActivate {
+  canActivate(context: ExecutionContext) {
+    const gqlContext = GqlExecutionContext.create(context).getContext();
+    const user = gqlContext['user'];
+    if (!user) return false;
+    return user.isManager;
+  }
+}
