78710af19630a7c13987be820dd6caf544246ea9
dv970406 <dv970406@gmail.com>
2023-02-14 17:06:14 +0900

    feat: Modify package and schema

--- front/relay.config.js
+++ front/relay.config.js
@@ -4,6 +4,4 @@ module.exports = {
   schema: "schema.graphql",
   exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
   language: "typescript",
-  // 아래 artifactDirectory설정 때문에 relay-compiler 커맨드 안먹히면 직접 그 경로에 __generated__ 폴더를 생성하고 해볼것
-  // artifactDirectory: "./__generated__",
 };
