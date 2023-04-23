d34d086789e5016dd94a372f3c5a63b42e4519ad
dv970406 <dv970406@gmail.com>
2023-04-23 20:38:39 +0900

    feat: Modify Organisms Survey

--- front/src/components/organisms/content/survey/SurveyTableContent.tsx
+++ front/src/components/organisms/content/survey/SurveyTableContent.tsx
@@ -9,17 +9,13 @@ import { useState } from "react";
 import { useFragment } from "react-relay";
 import { useNavigate } from "react-router-dom";
 import { theme } from "../../../../css/theme";
-import { getElaspedDay } from "../../../../utils/time/time";
-import {
-  RowBox,
-  HorizontalDivider,
-  ColumnBox,
-  ItemBox,
-} from "../../../atomics/boxes/Boxes";
-import { MainText } from "../../../atomics/typographys/texts";
-import { SectionTitle } from "../../../atomics/typographys/titles";
-import { IconButton } from "../../../molecules/buttons/IconButton";
-import { BoxIcon } from "../../../molecules/icons/BoxIcon";
+import { getElaspedDay } from "../../../../utils/shared/time";
+import { HorizontalDivider } from "../../../atomics/boxes/Divider";
+import { ColumnBox, RowBox } from "../../../atomics/boxes/FlexBox";
+import { GridItem } from "../../../atomics/boxes/GridBox";
+import { SectionTitle } from "../../../atomics/typographys/Etc";
+import { MainText } from "../../../atomics/typographys/Main";
+import { IconBox } from "../../../molecules/boxes/IconBox";
 import { SurveyTableContent_survey$key } from "./__generated__/SurveyTableContent_survey.graphql";
 
 interface ISurveyTableContent {
@@ -40,33 +36,39 @@ const surveyTableContentFragment = graphql`
 `;
 
 const SurveyTableContent = ({ survey }: ISurveyTableContent) => {
-  const tableContentData = useFragment(surveyTableContentFragment, survey);
+  const {
+    id: surveyId,
+    isAnonymous,
+    isAnswered,
+    surveyTitle,
+    user,
+    createdAt,
+  } = useFragment(surveyTableContentFragment, survey);
+
   const navigate = useNavigate();
-  const surveyCreatedAt = getElaspedDay(tableContentData.createdAt);
+  const surveyCreatedAt = getElaspedDay(createdAt);
 
   const [isHovering, setIsHovering] = useState(false);
 
   return (
-    <ItemBox
-      onClick={() => navigate(`/survey/${tableContentData.id}`)}
+    <GridItem
+      onClick={() => navigate(`/survey/${surveyId}`)}
       onMouseEnter={() => setIsHovering(true)}
       onMouseLeave={() => setIsHovering(false)}
       style={{
-        opacity: tableContentData.isAnswered ? 0.4 : 1,
+        opacity: isAnswered ? 0.4 : 1,
       }}
     >
-      <RowBox>
-        <BoxIcon
+      <RowBox style={{ width: "100%" }}>
+        <IconBox
           bgColor={isHovering ? theme.bgColors.purple : theme.bgColors.gray}
-          icon={tableContentData.isAnswered ? faBadgeCheck : faPenCircle}
+          icon={isAnswered ? faBadgeCheck : faPenCircle}
           size={"2x"}
         />
         <ColumnBox gap={theme.spacing.sm}>
-          <SectionTitle className="one-line">
-            {tableContentData.surveyTitle}
-          </SectionTitle>
+          <SectionTitle className="one-line">{surveyTitle}</SectionTitle>
 
-          <MainText>{tableContentData.user.name}</MainText>
+          <MainText>{user.name}</MainText>
         </ColumnBox>
       </RowBox>
 
@@ -75,20 +77,17 @@ const SurveyTableContent = ({ survey }: ISurveyTableContent) => {
         style={{
           justifyContent: "space-between",
           alignItems: "center",
+          width: "100%",
         }}
       >
-        <BoxIcon
-          icon={tableContentData.isAnonymous ? faEyeSlash : faEye}
-          bgColor={
-            tableContentData.isAnonymous
-              ? theme.bgColors.red
-              : theme.bgColors.purple
-          }
+        <IconBox
+          icon={isAnonymous ? faEyeSlash : faEye}
+          bgColor={isAnonymous ? theme.bgColors.red : theme.bgColors.purple}
         />
 
         <MainText>{surveyCreatedAt}</MainText>
       </RowBox>
-    </ItemBox>
+    </GridItem>
   );
 };
 
