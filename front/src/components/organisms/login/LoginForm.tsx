d3f0a7514d81e1bd62084e3756de6eb5886b7ebb
dv970406 <dv970406@gmail.com>
2023-04-27 16:13:22 +0900

    feat: Modify Organisms Login

--- front/src/components/organisms/login/LoginForm.tsx
+++ front/src/components/organisms/login/LoginForm.tsx
@@ -2,10 +2,10 @@ import { faLock, faUser } from "@fortawesome/pro-solid-svg-icons";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useLogin } from "../../../client/user/Login.client";
 import { theme } from "../../../css/theme";
-import { ColumnBox } from "../../atomics/boxes/FlexBox";
-import { Section } from "../../atomics/sections/sections";
+import { CenterBox, ColumnBox } from "../../atomics/boxes/FlexBox";
+import { Section } from "../../atomics/boxes/Sections";
 import { EndSubmitButton } from "../../molecules/buttons/EndSubmitButton";
-import Form from "../../molecules/form/Form";
+import Form from "../../molecules/shared/Form";
 import { TextInput } from "../../molecules/inputs/TextInput";
 
 interface ILoginForm {
@@ -39,63 +39,68 @@ const LoginForm = () => {
   return (
     <Section
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: theme.spacing.md,
         width: 500,
       }}
     >
-      <ColumnBox>
-        <ColumnBox style={{ alignItems: "center" }}>
-          <img src="/logo.png" width={170} height={30} />
+      <CenterBox
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: theme.spacing.md,
+        }}
+      >
+        <ColumnBox style={{ width: "100%" }}>
+          <ColumnBox style={{ alignItems: "center" }}>
+            <img src="/logo.png" alt="TIM_SOLUTION" width={170} height={30} />
+          </ColumnBox>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <TextInput
+              icon={faUser}
+              placeholder="email@timmanage.com"
+              type="email"
+              register={register("email", {
+                required: {
+                  value: true,
+                  message: "이메일을 입력해주세요.",
+                },
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: "이메일 형식이 아닙니다.",
+                },
+              })}
+              errorMessage={errors?.email && errors?.email.message}
+            />
+
+            <TextInput
+              icon={faLock}
+              placeholder="password"
+              type="password"
+              register={register("password", {
+                required: {
+                  value: true,
+                  message: "비밀번호를 입력해주세요.",
+                },
+                minLength: {
+                  value: 4,
+                  message: "비밀번호는 4글자 이상입니다.",
+                },
+                maxLength: {
+                  value: 16,
+                  message: "비밀번호는 16글자 이하입니다.",
+                },
+              })}
+              errorMessage={errors?.password && errors?.password.message}
+            />
+
+            <EndSubmitButton
+              onClick={handleSubmit(onSubmit)}
+              disabled={loginLoading || isSubmitDisabled}
+              text="LOGIN"
+            />
+          </Form>
         </ColumnBox>
-        <Form onSubmit={handleSubmit(onSubmit)}>
-          <TextInput
-            icon={faUser}
-            placeholder="email@timmanage.com"
-            type="email"
-            register={register("email", {
-              required: {
-                value: true,
-                message: "이메일을 입력해주세요.",
-              },
-              pattern: {
-                value: /\S+@\S+\.\S+/,
-                message: "이메일 형식이 아닙니다.",
-              },
-            })}
-            errorMessage={errors?.email && errors?.email.message}
-          />
-
-          <TextInput
-            icon={faLock}
-            placeholder="password"
-            type="password"
-            register={register("password", {
-              required: {
-                value: true,
-                message: "비밀번호를 입력해주세요.",
-              },
-              minLength: {
-                value: 4,
-                message: "비밀번호는 4글자 이상입니다.",
-              },
-              maxLength: {
-                value: 16,
-                message: "비밀번호는 16글자 이하입니다.",
-              },
-            })}
-            errorMessage={errors?.password && errors?.password.message}
-          />
-
-          <EndSubmitButton
-            onClick={handleSubmit(onSubmit)}
-            disabled={loginLoading || isSubmitDisabled}
-            text="LOGIN"
-          />
-        </Form>
-      </ColumnBox>
+      </CenterBox>
     </Section>
   );
 };
