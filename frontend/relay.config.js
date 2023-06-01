97159f70c1d9889b67c0959a43cec94e1156675f
dv970406 <dv970406@gmail.com>
2023-06-01 13:53:42 +0900

    docs: Delete Next13 Ver frontend

--- frontend/relay.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  src: "./src",
-  // 현재 schema 파일의 위치
-  schema: "schema.graphql",
-  exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
-  language: "typescript",
-};
--- frontend/src/components/atomics/typographys/texts.ts
+++
@@ -1,7 +0,0 @@
-import styled from "@emotion/styled";
-
-export const MainText = styled.p(({ theme }) => ({
-  color: theme.colors.white,
-  fontSize: theme.fonts.text,
-  fontWeight: theme.bold.text,
-}));
